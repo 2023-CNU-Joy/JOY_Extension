251ee4627f63ab8c83837d85c1272466ac3094a8
dlwnsrb0829 <ijg0829@naver.com>
2023-05-25 12:34:21 +0900

    ADD : extension code written by javascrit

---
+++ JOY/extension.js
@@ -0,0 +1,237 @@
+const vscode = require('vscode');
+const { spawn } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+var inputList = [];
+var outputList = [];
+var check;
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let get_problem = vscode.commands.registerCommand('JOY.get', function () {
+		const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		const testcaseFolderName = 'testcase';
+		const testcaseFolderPath = path.join(workspacePath, testcaseFolderName);
+		fs.mkdir(testcaseFolderPath, (err) => {
+		  	if (err) {
+				vscode.window.showErrorMessage('Failed to create testcase folder: ' + err.message);
+			} else {
+				vscode.window.showInformationMessage('Testcase folder created successfully');
+		  	}
+    	});
+	});
+
+
+	let remove_problem = vscode.commands.registerCommand('JOY.remove', function () {
+		const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+      	const testcaseFolderName = 'testcase';
+      	const testcaseFolderPath = path.join(workspacePath, testcaseFolderName);
+      	fs.rmdir(testcaseFolderPath, { recursive: true }, (err) => {
+        	if (err) {
+        		vscode.window.showErrorMessage('Failed to delete testcase folder: ' + err.message);
+        	} else {
+        		vscode.window.showInformationMessage('Testcase folder deleted successfully');
+        	}
+      	});
+	});
+
+
+	let get_result = vscode.commands.registerCommand('JOY.result', function () {
+		console.log(check)
+		if(check){
+			vscode.window.showInformationMessage("TestCase에 통과하였습니다.");
+		}else{
+			vscode.window.showErrorMessage(`TestCase에 통과하지 못하였습니다.`);
+		}
+	});
+
+
+	let show_problem = vscode.commands.registerCommand('JOY.show', function () {
+		const view = vscode.window.createWebviewPanel(
+			'joy.problem',
+			'JOY',
+			vscode.ViewColumn.One,
+			{
+			  enableScripts: true
+			}
+		);
+		let htmlContent = view.webview.html;
+		  // HTML 내용에 글씨를 추가합니다.
+		  htmlContent += '<h2>Additional Text</h2>';
+		view.webview.html = htmlContent;
+	});
+
+
+	function createInputTreeView(){
+		vscode.window.createTreeView('joy-input',{
+			treeDataProvider: new inputProvider()
+		});
+	}
+	//데이터 삽입
+	const input = new Input("a",vscode.TreeItemCollapsibleState.None);
+	inputList.push(input);
+
+	function createOutputTreeView(){
+		vscode.window.createTreeView('joy-output',{
+			treeDataProvider: new outputProvider()
+		});
+	}
+	//데이터 삽입
+	const output = new Output("a",vscode.TreeItemCollapsibleState.None);
+	outputList.push(output);
+
+
+	createInputTreeView();
+	createOutputTreeView();
+
+
+	let test = vscode.commands.registerCommand('JOY.test', function () {
+		const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            vscode.window.showErrorMessage('열린 파일을 찾을 수 없습니다.');
+            return;
+        }
+
+		const activeFilePath = activeEditor.document.fileName;
+		const input = [];
+		for(var i = 0 ; i < 10 ; i++){
+			const temp_input = path.join(path.dirname(activeFilePath), 'testcase/tcin'+i);
+			const input_value = readInputFromFile(temp_input);
+			input.push(input_value);
+		}
+
+        // main.c 파일의 상대 경로 계산
+        const filePath = path.join(path.dirname(activeFilePath), 'main.c');
+		const programPath = path.join(path.dirname(activeFilePath), 'program');
+		// vscode.window.showInformationMessage(filePath);
+        // 컴파일 명령어와 실행 인수 설정
+        const compileCommand = 'gcc';
+        const compileArgs = ['-o', programPath, filePath];  // 컴파일하여 'program'이라는 실행 파일 생성
+
+        // 컴파일 후 프로그램 실행
+        const compileProcess = spawn(compileCommand, compileArgs);
+
+        compileProcess.stdout.on('data', (data) => {
+            console.log(`컴파일 출력: ${data}`);
+        });
+
+        compileProcess.stderr.on('data', (data) => {
+            console.error(`컴파일 에러: ${data}`);
+        });
+
+        compileProcess.on('close', (code) => {
+			check = true;
+            if (code === 0) {
+                console.log('컴파일 완료');
+
+				for(var i = 0 ; i < 10 ; i++){
+					// 컴파일이 성공적으로 완료되면 프로그램 실행
+					var runCommand = path.join(path.dirname(activeFilePath), 'program');
+					const runProcess = spawn(runCommand);
+
+					//표준 입력을 위한 stdin
+					runProcess.stdin.write(input[i]);
+					// console.log(i+" "+input[i]);
+					runProcess.stdin.end();
+
+					const temp_output = path.join(path.dirname(activeFilePath), 'testcase/tcout'+i);
+					const output_value = readInputFromFile(temp_output);
+					runProcess.stdout.on('data',async (data) => {
+						console.log(data.toString()+" "+ output_value)
+						if(data.toString() == output_value){
+							await handleTestCaseResult(true);
+						}else{
+        					await handleTestCaseResult(false);
+							check = false;
+						}
+					});
+					runProcess.stderr.on('data', (data) => {
+						console.error(`프로그램 에러: ${data}`);
+					});
+
+					runProcess.on('close', (code) => {
+						console.log(`프로그램 종료, 종료 코드: ${code}`);
+					});
+				}
+            } else {
+                console.error('컴파일 에러');
+            }
+        });
+		
+	});
+
+	async function handleTestCaseResult(passed) {
+		if (passed) {
+			// vscode.window.showInformationMessage("TestCase에 통과하였습니다.");
+		} else {
+			vscode.window.showErrorMessage('TestCase에 통과하지 못하였습니다.');
+			check = false;
+		}
+	}
+
+	context.subscriptions.push(get_problem);
+	context.subscriptions.push(remove_problem);
+	context.subscriptions.push(show_problem);
+	context.subscriptions.push(get_result);
+	context.subscriptions.push(test);
+}
+
+function deactivate() {}
+
+function readInputFromFile(filePath) {
+	try {
+	  const absolutePath = filePath;
+	  const input = fs.readFileSync(absolutePath, 'utf-8');
+	  return input;
+	} catch (error) {
+	  console.error(`Error reading input file: ${error}`);
+	  return null;
+	}
+}
+
+class inputProvider{
+	getTreeItem(element) {
+        return element;
+    }
+	getChildren(element){
+        const temp = Object.assign([], inputList);
+        // return Promise.resolve(temp.reverse());
+		return Promise.resolve(temp);
+    }
+}
+class Input extends vscode.TreeItem {
+    constructor(
+        label,
+        collapsibleState
+    ) {
+        super(label, collapsibleState);
+    }
+}
+
+class outputProvider{
+	getTreeItem(element) {
+        return element;
+    }
+	getChildren(element){
+        const temp = Object.assign([], outputList);
+        // return Promise.resolve(temp.reverse());
+		return Promise.resolve(temp);
+    }
+}
+class Output extends vscode.TreeItem {
+    constructor(
+        label,
+        collapsibleState
+    ) {
+        super(label, collapsibleState);
+    }
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
