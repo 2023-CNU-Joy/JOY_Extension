4c4934444e0fdd90d57c1038897311cd796969fb
dlwnsrb0829 <ijg0829@naver.com>
2023-05-27 23:21:43 +0900

    UPDATE : Connect Server and Use API

--- JOY/extension.js
+++ JOY/extension.js
@@ -2,14 +2,19 @@ const vscode = require('vscode');
 const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios').default;
+const CryptoJS = require('crypto-js');
 
-var inputList = [];
-var outputList = [];
+
+var sampleList = [];
 var check = true;
 
-var NumberOfTest = 5;
+var NumberOfTest = 0;
+var NumberOfTestcase = [];
 
 var problems = [];
+
+const AESKEY = "0123456789abcdef0123456789abcdef";
 
 
 /**
@@ -21,11 +26,71 @@ function activate(context) {
 
 	const server_ip = config.get("serverIP");
 	const ID = config.get("ID");
-	console.log("asd" + server_ip);
-	console.log("asd" + ID);
+	console.log(server_ip);
+	console.log(ID);
 
-	async function makeTestCase(i){
-		var path1 = vscode.workspace.workspaceFolders[0].uri.fsPath + "/test"+i;
+
+	async function fetchProblem() {
+		try {
+		  	const response = await axios.get(server_ip+"/api/v1/problems");
+		  	const data = response.data;
+		  	console.log(data);
+		//   var ddata = JSON.stringify();
+		  	NumberOfTest = data.length;
+
+		  	const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+			const testFolderName = 'test';
+			const testFolderPath = path.join(workspacePath, testFolderName);
+			for(var i = 0 ; i < NumberOfTest ; i++){
+				fs.mkdirSync(testFolderPath + i);
+				await makeTestCase(i);
+				var problem = {
+					id : i,
+					check : false,
+					path : testFolderPath,
+					isCompile : false,
+					title : data[i].title,
+					content : data[i].content,
+					testInputSample : data[i].exampleTestInput,
+					testOutputSample : data[i].exampleTestOut
+				};
+				problems.push(problem);
+			}
+		  // 데이터를 활용하여 추가적인 작업 수행
+		} catch (error) {
+		  console.error('API 호출 중 오류가 발생했습니다:', error.message);
+		}
+	}
+
+	async function fetchTestcase() {
+		try {
+		  	const response = await axios.get(server_ip+"/api/v1/testcases");
+		  	const data = response.data;
+		  	console.log(data);
+			for(var i = 0 ; i < data.length ; i++){
+				for(var j = 0 ; j < data[i].length ; j++){
+					for(var k = 0 ; k < data[i][j].length ; k++){
+						console.log(decodeByAES256(AESKEY, data[i][j][k]))
+					}
+				}
+			}
+		  // 데이터를 활용하여 추가적인 작업 수행
+		} catch (error) {
+		  	console.error('API 호출 중 오류가 발생했습니다:', error.message);
+		}
+	}
+
+	function decodeByAES256(key, data){
+		const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+			iv: CryptoJS.enc.Utf8.parse(""),
+			padding: CryptoJS.pad.Pkcs7,
+			mode: CryptoJS.mode.CBC
+		});
+		return cipher.toString(CryptoJS.enc.Utf8);
+	};
+
+	async function makeTestCase(num){
+		var path1 = vscode.workspace.workspaceFolders[0].uri.fsPath + "/test"+num;
 		const testCaseFolderName = 'testcase';
 		const testCaseFolderPath = path.join(path1, testCaseFolderName);
 		fs.mkdir(testCaseFolderPath, (err) => {
@@ -35,39 +100,71 @@ function activate(context) {
 				vscode.window.showInformationMessage('Testcase folder created successfully');
 		  	}
 		});
+
+		const mainPath = path.join(path1, "main.c");
+		fs.writeFileSync(mainPath, '');
+		
+		try {
+			const response = await axios.get(server_ip+"/api/v1/testcases");
+			const data = response.data;
+			// console.log(data);
+			var path2 = vscode.workspace.workspaceFolders[0].uri.fsPath + "/test"+num+"/testcase";
+			for(var j = 0 ; j < data[num][0].length ; j++){
+				const testCasePath = path.join(path2, "tcin"+j);
+				fs.writeFileSync(testCasePath, data[num][0][j]);
+			}
+			for(var j = 0 ; j < data[num][0].length ; j++){
+				const testCasePath = path.join(path2, "tcout"+j);
+				fs.writeFileSync(testCasePath, data[num][1][j]);
+			}
+		// 데이터를 활용하여 추가적인 작업 수행
+	  } catch (error) {
+			console.error('API 호출 중 오류가 발생했습니다:', error.message);
+	  }
 	}
 
 	let get_problem = vscode.commands.registerCommand('JOY.get', async function () {
-		const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-		const testFolderName = 'test';
-		const testFolderPath = path.join(workspacePath, testFolderName);
-		for(var i = 0 ; i < NumberOfTest ; i++){
-			fs.mkdirSync(testFolderPath + i);
-			await makeTestCase(i);
-			var problem = {
-				id : i,
-				check : false,
-				path : testFolderPath,
-				isCompile : false
-			};
-			problems.push(problem);
+		await fetchProblem();
+
+		try {
+			const response = await axios.get(server_ip+"/api/v1/testcases");
+			const data = response.data;
+			console.log(data);
+			for(var i = 0 ; i < NumberOfTest ; i++){
+				NumberOfTestcase.push(data[i][0].length);
+			}
+		// 데이터를 활용하여 추가적인 작업 수행
+	  	} catch (error) {
+			console.error('API 호출 중 오류가 발생했습니다:', error.message);
+	  	}
+
+		function createTestcaseSampleTreeView(){
+			vscode.window.createTreeView('joy-InOut',{
+				treeDataProvider: new testcaseSampleProvider()
+			});
 		}
+		//데이터 삽입
+		for(var i = 0 ; i < NumberOfTest ; i++){
+			var inputArray  = problems[i].testInputSample[0].split('\n')
+			var outputArray  = problems[i].testOutputSample[0].split('\n')
+			const test = new TestcaseSample("test" + i, vscode.TreeItemCollapsibleState.Collapsed);
+			const input = new TestcaseSample("Input",vscode.TreeItemCollapsibleState.Collapsed);
+			const output = new TestcaseSample("Output",vscode.TreeItemCollapsibleState.Collapsed);
+			test.addChild(input);
+			test.addChild(output);
+			for(var j = 0 ; j < inputArray.length ; j++){
+				const testcase = new TestcaseSample(inputArray[j],vscode.TreeItemCollapsibleState.None);
+				input.addChild(testcase);
+			}
+			for(var j = 0 ; j < outputArray.length ; j++){
+				const testcase = new TestcaseSample(outputArray[j],vscode.TreeItemCollapsibleState.None);
+				output.addChild(testcase);
+			}
+			sampleList.push(test)
+		}
+		createTestcaseSampleTreeView();
+
 	});
-
-
-	let remove_problem = vscode.commands.registerCommand('JOY.remove', function () {
-		// const workspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-      	// const testcaseFolderName = 'testcase';
-      	// const testcaseFolderPath = path.join(workspacePath, testcaseFolderName);
-      	// fs.rmdir(testcaseFolderPath, { recursive: true }, (err) => {
-        // 	if (err) {
-        // 		vscode.window.showErrorMessage('Failed to delete testcase folder: ' + err.message);
-        // 	} else {
-        // 		vscode.window.showInformationMessage('Testcase folder deleted successfully');
-        // 	}
-      	// });
-	});
-
 
 	let get_result = vscode.commands.registerCommand('JOY.result', function () {
 		const activeEditor = vscode.window.activeTextEditor;
@@ -85,20 +182,21 @@ function activate(context) {
 		}
 	});
 
-
 	let show_problem = vscode.commands.registerCommand('JOY.show', function () {
-		const view = vscode.window.createWebviewPanel(
-			'joy.problem',
-			'JOY',
-			vscode.ViewColumn.Beside,
-			{
-			  enableScripts: true
-			}
-		);
-		let htmlContent = view.webview.html;
-		  // HTML 내용에 글씨를 추가합니다.
-		  htmlContent += '<h2>Additional Text</h2>';
-		view.webview.html = htmlContent;
+		for(var i = 0 ; i < NumberOfTest ; i++){
+			const view = vscode.window.createWebviewPanel(
+				'joy.problem',
+				'test'+i,
+				vscode.ViewColumn.Two,
+				{
+				  enableScripts: true
+				}
+			);
+			let htmlContent = view.webview.html;
+			  // HTML 내용에 글씨를 추가합니다.
+			  htmlContent += '<h2>'+problems[i].title+'</h2>'+problems[i].content;
+			view.webview.html = htmlContent;
+		}
 	});
 
 	let send_result = vscode.commands.registerCommand('JOY.send', function () {
@@ -111,24 +209,6 @@ function activate(context) {
 		}
 	});
 
-
-	function createTestcaseSampleTreeView(){
-		vscode.window.createTreeView('joy-InOut',{
-			treeDataProvider: new testcaseSampleProvider()
-		});
-	}
-	//데이터 삽입
-	for(var i = 0 ; i < NumberOfTest ; i++){
-		const test = new TestcaseSample("test" + i, vscode.TreeItemCollapsibleState.Collapsed);
-		const input = new TestcaseSample("Input",vscode.TreeItemCollapsibleState.Collapsed);
-		const output = new TestcaseSample("Output",vscode.TreeItemCollapsibleState.Collapsed);
-		test.addChild(input);
-		test.addChild(output);
-		outputList.push(test)
-	}
-	createTestcaseSampleTreeView();
-
-
 	let test = vscode.commands.registerCommand('JOY.test', function () {
 		const activeEditor = vscode.window.activeTextEditor;
         if (!activeEditor) {
@@ -138,10 +218,13 @@ function activate(context) {
 
 		const activeFilePath = activeEditor.document.fileName;
 		const input = [];
-		for(var i = 0 ; i < 10 ; i++){
+
+		const problemNum = (path.dirname(activeFilePath)).slice(-1);
+
+		for(var i = 0 ; i < NumberOfTestcase[problemNum] ; i++){
 			const temp_input = path.join(path.dirname(activeFilePath), 'testcase/tcin'+i);
 			const input_value = readInputFromFile(temp_input);
-			input.push(input_value);
+			input.push(decodeByAES256(AESKEY, input_value));
 		}
 
         // main.c 파일의 상대 경로 계산
@@ -167,7 +250,11 @@ function activate(context) {
             if (code === 0) {
                 console.log('컴파일 완료');
 
-				for(var i = 0 ; i < 10 ; i++){
+				const activeEditor = vscode.window.activeTextEditor;
+				const activeFilePath = activeEditor.document.fileName;
+				const problemNum = (path.dirname(activeFilePath)).slice(-1);
+
+				for(var i = 0 ; i < NumberOfTestcase[problemNum] ; i++){
 					// 컴파일이 성공적으로 완료되면 프로그램 실행
 					var runCommand = path.join(path.dirname(activeFilePath), 'program');
 					const runProcess = spawn(runCommand);
@@ -180,7 +267,7 @@ function activate(context) {
 					const output_value = readInputFromFile(temp_output);
 					runProcess.stdout.on('data',async (data) => {
 						// console.log(data.toString()+" "+ output_value)
-						if(data.toString() == output_value){
+						if(data.toString() == decodeByAES256(AESKEY, output_value)){
 							await handleTestCaseResult(true);
 						}else{
         					await handleTestCaseResult(false);
@@ -203,13 +290,11 @@ function activate(context) {
 
 	async function handleTestCaseResult(passed) {
 		if (passed) {
-			// vscode.window.showInformationMessage("TestCase에 통과하였습니다.");
 			const activeEditor = vscode.window.activeTextEditor;
 			const activeFilePath = activeEditor.document.fileName;
 			const problemNum = (path.dirname(activeFilePath)).slice(-1);
 			problems[problemNum].isCompile = true;
 		} else {
-			//vscode.window.showErrorMessage('TestCase에 통과하지 못하였습니다.');
 			const activeEditor = vscode.window.activeTextEditor;
 			const activeFilePath = activeEditor.document.fileName;
 			const problemNum = (path.dirname(activeFilePath)).slice(-1);
@@ -219,7 +304,6 @@ function activate(context) {
 	}
 
 	context.subscriptions.push(get_problem);
-	// context.subscriptions.push(remove_problem);
 	context.subscriptions.push(show_problem);
 	context.subscriptions.push(get_result);
 	context.subscriptions.push(test);
@@ -246,7 +330,7 @@ class testcaseSampleProvider{
 	getChildren(element){
         if (!element) {
 			// Root 노드의 하위 항목 반환
-			return Promise.resolve(outputList);
+			return Promise.resolve(sampleList);
 		} else {
 			// 각 항목의 하위 항목 반환
 			return Promise.resolve(element.children);
